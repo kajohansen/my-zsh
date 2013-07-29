0832d7ec30fc02ceb5a15a7f6a4c118b95ec1991
Karl Andreas <kajohansen.1@gmail.com>
2013-07-29 11:32:57 +0200

    Modified to reflect OSX paths

--- custom/example.zsh
+++ custom/example.zsh
@@ -4,12 +4,12 @@
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
 # Mac OS X paths
-# webroot=/Users/superuser/Sites
-# httproot=/etc/apache2
-# pearhome=/usr/lib/php/pear
+webroot=/Users/superuser/Sites
+httproot=/etc/apache2
+pearhome=/usr/lib/php/pear
 
 # Amazon Linux Paths
-webroot=/var/www/html
-httproot=/etc/httpd
-pearroot=/usr/share/pear
-svnroot=/srv/Subversion
+# webroot=/var/www/html
+# httproot=/etc/httpd
+# pearroot=/usr/share/pear
+# svnroot=/srv/Subversion
