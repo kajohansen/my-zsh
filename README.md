4fcb23a48e5066d02e7c4a710b70c3a41ef4ad4f
Karl Andreas <kaepost123@gmail.com>
2020-02-20 15:37:13 +0100

    Removed sudo prefix

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 my-zsh
 ======
 
-my custom oh-my-zsh
+##**my custom oh-my-zsh**
+
+the master brach is meant to be installed on a system with an active root account
+*(no sudo prefix on any commands in this branch)*
--- plugins/macports/macports.plugin.zsh
+++ plugins/macports/macports.plugin.zsh
@@ -1,8 +1,8 @@
 #Aliases
-alias pc="sudo port clean --all installed"
-alias pi="sudo port install $1"
-alias psu="sudo port selfupdate"
-alias puni="sudo port uninstall inactive"
-alias puo="sudo port upgrade outdated"
+alias pc="port clean --all installed"
+alias pi="port install $1"
+alias psu="port selfupdate"
+alias puni="port uninstall inactive"
+alias puo="port upgrade outdated"
 alias pup="psu && puo"
 
